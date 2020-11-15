8872ebbf25b8861754d9cc158a41cda0f00429d9
bste1220 <bste1146@gmail.com>
2020-11-15 15:01:44 +0900

    1115

---
+++ 1115/BOJ_10699_오늘 날짜.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+now = datetime.now()
+# 현재 날짜
+nowDate = now.strftime('%Y-%m-%d')
+print(nowDate)
+# 현재 시간
+# nowTime = now.strftime('%H:%M:%S')
+
+
+
+# 현재날짜시간
+# nowDateTime = now.strftime('%Y-%m-%d %H:%M:%S')
